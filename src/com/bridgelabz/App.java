54bb8f1a4e09b8a8b50054177c58daf034f4f948
JAYESH <jayjdahivale@gmail.com>
2022-03-10 11:15:48 +0530

    Added UC1

--- src/com/bridgelabz/App.java
+++ src/com/bridgelabz/App.java
@@ -1,10 +1,21 @@
 package com.bridgelabz;
-
+import java.util.Random;
 public class App {
-    
+	
+    static public void checkAttendance() {
+    	Random randomInt=new Random();
+    	int randInt=randomInt.nextInt(2);
+    	if(randInt==1) {
+    		System.out.println("Employee is present");
+    	}
+    	else {
+    		System.out.println("Employee is absent");
+    	}
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Welcome to Employee wage computation program");
+       checkAttendance();
 	}
 
 }
