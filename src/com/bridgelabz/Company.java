c5d856fbe47f51a876e59c186c3e2684f99c763c
JAYESH <jayjdahivale@gmail.com>
2022-03-11 21:19:04 +0530

    Added UC8

---
+++ src/com/bridgelabz/Company.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class Company {
+   String name;
+   int wagePerHour;
+   int workingDays;
+   int workingHoursMonthly;
+   void setInfo(String name,int wagePerHour,int workingDays,int workingHoursMonthly) {
+	   this.name=name;
+	   this.wagePerHour=wagePerHour;
+	   this.workingDays=workingDays;
+	   this.workingHoursMonthly=workingHoursMonthly;
+	   
+   }
+}
